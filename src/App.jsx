8d66cfcc8fb1f6b5916543760e320d8bef27ecbb
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-09-06 12:42:04 +0530

    vercel analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import BackgroundGen from "./components/BackgroundGen";
 import GradientGen from "./components/GradientGen";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <div>
       <BackgroundGen />
       <GradientGen />
+      <Analytics />
     </div>
   );
 };
